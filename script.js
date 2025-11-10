74ad511a7ea0e0297d1f6f4496dfd2c916ae7b65
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-11-10 17:21:56 +1300

    slideshow in progress

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const hamburgerBtn = document.getElementById("hamburger");
 const closeBtn = document.getElementById("close");
 const mobileMenu = document.getElementById("mobile-menu");
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+
+}
+
 const sideBar = () => {
   mobileMenu.style.width = "250px";
 };
